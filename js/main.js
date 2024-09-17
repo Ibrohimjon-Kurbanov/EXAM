dc12422eac389653118af6f5854842b72e1fe0e9
Ibrohimjon-Kurbanov <kurbanovolddev@gmail.com>
2024-09-17 23:50:36 +0200

    3-oy-imtixon

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ function closepreloader() {
 window.addEventListener("load", function () {
   setTimeout(closepreloader, 1000);
 });
+// SCROLL ANIMATION
+window.addEventListener("scroll", function () {
+  var anime = document.querySelectorAll(".animeX");
+
+  for (var s = 0; s < anime.length; s++) {
+    var windowheight = window.innerHeight;
+    var animetop = anime[s].getBoundingClientRect().top;
+    var animepoint = 150;
+
+    if (animetop < windowheight - animepoint) {
+      anime[s].classList.add("active");
+    } else {
+      anime[s].classList.remove("active");
+    }
+  }
+});
